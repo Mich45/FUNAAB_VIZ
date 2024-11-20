4d6c4827f1e58c3050b5474b3ce4bdceba28d1f7
Michael Hungbo <hungbomichael@gmail.com>
2024-11-21 00:25:30 +0100

    update

--- first-shiny-app/app.R
+++ first-shiny-app/app.R
@@ -4,6 +4,8 @@ library(readxl)
 
 graduation_list <- read_excel("/home/michael-hungbo/code/Shiny/first-shiny-app/NYSC Second 202324CURRENT.xlsx") %>%
   distinct() 
+View(graduation_list)
+
 total_graduates <- nrow(graduation_list)
 first_class_count <- nrow(graduation_list[graduation_list$ClassOfGrade == "First Class", ])
 second_class_upper_count <-  nrow(graduation_list[graduation_list$ClassOfGrade == "Second Class Upper", ])
@@ -11,6 +13,14 @@ second_class_lower_count <- nrow(graduation_list[graduation_list$ClassOfGrade ==
 first_class_percentage <- (first_class_count / total_graduates) * 100
 second_class_upper_percentage <- (second_class_upper_count / total_graduates) * 100
 second_class_lower_percentage <- (second_class_lower_count / total_graduates) * 100
+
+# Filter data for first-class graduates and count by department
+department_counts <- graduation_list %>%
+  filter(ClassOfGrade == "First Class") %>%
+  group_by(DeptmentCode) %>%
+  summarise(first_class_count = n()) %>%
+  arrange(desc(first_class_count)) %>%
+  head(10)
 
 
 ui <- dashboardPage(
@@ -22,6 +32,16 @@ ui <- dashboardPage(
       valueBoxOutput("firstClassPercentage", width = 3),
       valueBoxOutput('secondClassUpperPercentage', width = 3),
       valueBoxOutput("secondClassLowerPercentage", width = 3),
+  ),
+  fluidRow(
+    # Add a plot output for the bar plot
+    box(
+      title = "Top 10 Departments with Highest Number of First-Class Graduates",
+      status = "primary",
+      solidHeader = TRUE,
+      collapsible = TRUE,
+      plotOutput("barPlot")
+    )
   )
 )
 )
@@ -65,6 +85,26 @@ server <- function(input, output) {
       color = "red"
     )
   })
+  
+  # Render the horizontal bar plot
+  output$barPlot <- renderPlot({
+    ggplot(department_counts, aes(x = reorder(DeptmentCode, -first_class_count),
+                                  y = first_class_count, 
+                                  fill = first_class_count)) +  # Map fill to first_class_count
+      geom_bar(stat = "identity") +
+      coord_flip() +  # Flip coordinates for horizontal bars
+      scale_fill_gradient(low = "lightblue", high = "steelblue") +
+      geom_text(aes(label = first_class_count), 
+                hjust = -0.2,  
+                size = 4,      
+                color = "black") + 
+      labs(x = "Department", y = "Number of First Class Graduates") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  
+  
+  
 }
 
 shinyApp(ui, server)
